13f821765c70926261a8389f1d7638845dc8fcb7
darmaigabor <darmai.gabor@alerant.hu>
2016-04-26 16:48:16 +0200

    Added files via upload

---
+++ ConversationsCache.java
@@ -0,0 +1,80 @@
+package hu.alerant.signalingserver.cache;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.infinispan.Cache;
+import org.infinispan.manager.CacheContainer;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
+@Component("applicationCache")
+@Scope("singleton")
+public class ConversationsCache {
+
+	private CacheContainer container;
+
+	private Cache<String, ConversationCacheEntry> cache;
+
+	@PostConstruct
+	public void initCache() throws Exception {
+		container = (CacheContainer)new javax.naming.InitialContext().lookup("java:jboss/infinispan/container/applicationCache");
+		this.cache = container.getCache();
+	}
+
+	public HashMap<String, List<String>> listConversations() {
+		HashMap<String, List<String>> map = new HashMap();
+		for(ConversationCacheEntry c : this.cache.values()) {
+			map.put(c.conversationName, c.members);
+		}
+		return map;
+	}
+
+	public HashMap<String, HashMap<String, List<String>>> listConversationsByNodes() {
+		HashMap<String, HashMap<String, List<String>>> map = new HashMap();
+		for(ConversationCacheEntry c : this.cache.values()) {
+			HashMap<String, List<String>> conversations = map.get(c.ownerHost);
+			if(conversations == null) {
+				conversations = new HashMap();
+				map.put(c.ownerHost, conversations);
+			}
+			conversations.put(c.conversationName, c.members);
+		}
+		return map;
+	}
+
+	public void createConversation(String name) {
+		ConversationCacheEntry c = new ConversationCacheEntry(name);
+		this.cache.put(name, c);
+	}
+
+	public void removeConversation(String name) {
+		this.cache.remove(name);
+	}
+
+	public void addMemberToConversation(String name, String member) {
+		ConversationCacheEntry c = (ConversationCacheEntry)this.cache.get(name);
+		if(c != null) {
+			c.members.add(member);
+			this.cache.put(name, c);
+		}
+	}
+
+	public void removeMemberFromConversation(String name, String member) {
+		ConversationCacheEntry c = (ConversationCacheEntry)this.cache.get(name);
+		if(c != null) {
+			c.members.remove(member);
+			if(c.members.size() == 0) {
+				removeConversation(String name);
+			}
+			else {
+				this.cache.put(name, c);
+			}
+		}
+	}
+
+}
